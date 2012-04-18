529499f7f924b466cd30bfea103275243006d89c
Shay Davidson <shay.h.davidson@gmail.com>
2012-04-18 08:08:39 -0700

    generated rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FTW::Application.config.secret_token = 'f929566641d435dbdd5252594e33c776df877e8790f78a4065ee1f806e5c19f1c48a8cc77b1557f4cfcfe480e8e777f7900cec6f2fec3a58431c00302b18bc8d'
