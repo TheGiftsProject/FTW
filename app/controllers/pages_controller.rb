9cad65f98665303d5dbb0c6c31204b4cf087df09
yon <Lighthawky@gmail.com>
2012-04-21 13:34:19 +0300

    basic user support - login logout

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,21 @@
 class PagesController < ApplicationController
+
+  before_filter :must_be_logged_in, :only => [:dashboard]
+
   def landing
+  end
+
+  def dashboard
+
+
+  end
+
+
+  private
+
+  def must_be_logged_in
+    unless user_signed_in?
+      redirect_to '/'
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ FTW::Application.routes.draw do
 
   resource :session
 
+  match "dashboard" => "pages#dashboard", :as => "dashboard"
   root :to => 'pages#landing'
 
 end
