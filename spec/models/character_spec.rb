70bf8ab26203e47d7ff01f0dc2c5baf20ce96198
yon <Lighthawky@gmail.com>
2012-04-21 16:07:23 +0300

    added character partial

--- spec/models/character_spec.rb
+++ spec/models/character_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe Character do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe :needed_exp do
+    it "should return 0 for level 0" do
+      Character.needed_exp(0).should == 0
+    end
+
+    it "should return needed exp for given level" do
+      Character.needed_exp(1).should == 128
+      Character.needed_exp(2).should == 256
+      Character.needed_exp(3).should == 512
+    end
+
+  end
 end
