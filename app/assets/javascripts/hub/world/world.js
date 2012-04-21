2467df8df5962bb7e39c4e988c4f9f86845d88b4
Shay Davidson <shay.h.davidson@gmail.com>
2012-04-21 05:12:27 -0700

    added characters

--- app/assets/javascripts/hub/world/world.js
+++ app/assets/javascripts/hub/world/world.js
@@ -1,9 +1,12 @@
 function World(width, height) {
-    this.entities = {};
+
+    char = new Character(this, 96, 96);
+
+    this.characters = [char];
     this.width = width;
     this.height = height;
 }
 
 World.prototype.update = function(dt) {
-
+    _.each(this.characters, function(character) {character.update(); });
 }
